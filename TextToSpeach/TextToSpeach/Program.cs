845dacd7d877cba48a761b8bb270178848aff1ab
robhat75 <59373107+robhat75@users.noreply.github.com>
2019-12-30 16:12:07 -0500

    Hatfield's Test to Speech code

---
+++ TextToSpeach/TextToSpeach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Speech.Synthesis;
+
+//******************************************************
+// Author: Rob Hatfield
+// Description: This program uses microsoft .net to 
+//              convert text to speech
+//*****************************************************
+namespace TextToSpeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            //synth.SetOutputToDefaultAudioDevice(); 
+            foreach (var v in synth.GetInstalledVoices().Select(v => v.VoiceInfo))
+            {
+                Console.WriteLine("Name:{0}, Gender:{1}, Age:{2}",
+                  v.Description, v.Gender, v.Age);
+            }
+            synth.SelectVoice("Microsoft Zira Desktop");
+            Console.WriteLine("We are using " + synth.Voice.Name);
+            synth.SetOutputToDefaultAudioDevice();
+
+            // Speak a string.  
+            synth.Speak("This is a voice example for Shelby Valley's robotics project!");
+
+        }
+    }
+}
